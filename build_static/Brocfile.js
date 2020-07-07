6170d82b211388d384509898acd54488c2940ee6
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-07 13:27:12 -0700

    try adding annotations

--- build_static/Brocfile.js
+++ build_static/Brocfile.js
@@ -16,21 +16,24 @@ export default () => {
   let imgDir = new Funnel(htdocs, {
     srcDir: 'img',
     destDir: 'img',
+    annotation: 'Image dir (copy)',
   });
 
   // Vanilla ES5 JS: send it through the ES5 uglifier, if in prod.
   let jsDir = new Funnel(htdocs, {
     srcDir: 'js',
-    destDir: 'js'
+    destDir: 'js',
+    annotation: 'JS dir (copy)',
   });
   if (process.env.NODE_ENV === 'production') {
-    jsDir = uglify(jsDir);
+    jsDir = uglify(jsDir, {annotation: 'JS dir (uglify)'});
   }
 
   // Vanilla CSS (plus some maybe weird stuff?): just set it down over there, don't jostle it too hard.
   let stcDir = new Funnel(htdocs, {
     srcDir: 'stc',
-    destDir: 'stc'
+    destDir: 'stc',
+    annotation: 'stc dir (copy)',
   });
 
   // SCSS: start w/ an isolated working directory, so the include paths work out
@@ -39,9 +42,11 @@ export default () => {
   let scssDir = new Funnel(htdocs, {
     srcDir: 'scss',
     destDir: '.',
+    annotation: 'SCSS dir (copy to root)',
   });
   let sassOptions = {
     fiber: Fiber, // slightly faster.
+    annotation: 'SCSS dir (convert all)',
   };
   if (process.env.NODE_ENV === 'production') {
     sassOptions.outputStyle = 'compressed';
@@ -50,7 +55,10 @@ export default () => {
   let scssFinal = new Funnel(scssOutput, {
     srcDir: '.',
     destDir: 'stc/css',
+    annotation: 'SCSS dir (copy to stc/css)',
   });
 
-  return merge([imgDir, jsDir, stcDir, scssFinal]);
+  return merge([imgDir, jsDir, stcDir, scssFinal], {
+    annotation: 'Final merge',
+  });
 }
