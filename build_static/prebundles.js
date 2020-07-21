c0506be39674e52dc1357e31e07983ba9638317e
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-20 19:01:02 -0700

    More prebundling... I think I'm at the point where I just need to mull some of this. MULL FOLLOWS:
    - wondering how to deal with source files that are in multiple bundles. Thinking
    something like:
        - Float bundles to the top and process them first.
            - (ID them by filename? like, I'm bundling things into a separate
            top-level htdocs directory?)
        - While processing bundles, it's an error if we included two bundles that
        have the same file. Bundles must not overlap!
            - UGH, does that gotta be at runtime?? 😖 can't think of a way to do it
            during static compilation.
        - Once we finish bundles, if a resource that got requested is in one of the
        bundles we already have, skip it.
        - Include all "loosy" resources as individual link tags.
    - Maintaining these bundle lists is going to be a pain in the ass.
        - reporting on which bundles have overlap?
        - Codify categories of bundles? That way you can have one "lib" bundle and
        one "app" bundle, and overlaps across categories at all are illegal...
        well, maybe there are cases where you'd need two app bundles. Hmm.
    - How to handle skins? is each skin its own lib bundle, for CSS? (for js I think
    they mostly don't differ...)

--- build_static/prebundles.js
+++ build_static/prebundles.js
@@ -25,30 +25,39 @@ let prebundles = {
 
         'js/jquery/jquery.ui.core.js',
         'js/jquery/jquery.ui.widget.js',
-
         'js/jquery/jquery.ui.tooltip.js',
-        'js/jquery.ajaxtip.js',
+        'js/jquery/jquery.ui.button.js',
+        'js/jquery/jquery.ui.dialog.js',
         'js/jquery/jquery.ui.position.js',
-        'stc/jquery/jquery.ui.core.css',
-        'stc/jquery/jquery.ui.tooltip.css',
+        'js/jquery.ajaxtip.js',
 
         'js/jquery.hoverIntent.js',
         'js/jquery.contextualhover.js',
-        'stc/jquery.contextualhover.css',
 
     ],
 
-    base_foundation_css: [
+    base_foundation_nonskin_css: [
         'stc/lj_base.css',
+        'stc/esn.css',
+        'stc/css/foundation/foundation_minimal.css',
 
+        'stc/jquery/jquery.ui.core.css',
+        'stc/jquery/jquery.ui.tooltip.css',
+        'stc/jquery/jquery.ui.button.css',
+        'stc/jquery/jquery.ui.dialog.css',
+
+        'stc/jquery/jquery.ui.theme.smoothness.css',
+
+        'stc/jquery.contextualhover.css',
     ],
 
     base_foundation_siteskins_js: [
-
+        // Needs to differ per each skin, right? Or what?
     ],
 
     base_foundation_siteskins_css: [
         'stc/lj_base.css',
+        'stc/esn.css',
 
     ],
 
@@ -80,6 +89,7 @@ let prebundles = {
     ],
 
     journal_content_js: [
+        'js/jquery.esn.js',
         'js/jquery.replyforms.js',
         'js/jquery.poll.js',
         'js/journals/jquery.tag-nav.js',
@@ -90,12 +100,16 @@ let prebundles = {
         'js/jquery.talkform.js',
         'js/jquery.cuttag-ajax.js',
         'js/jquery.default-editor.js',
+        'js/jquery.commentmanage.js',
+
+
     ],
 
     journal_content_css: [
         'stc/css/components/quick-reply.css',
-        'stc/css/components/imageshrink.css',
         'stc/css/components/talkform.css',
+        'stc/css/components/imageshrink.css',
+        'stc/jquery.commentmanage.css',
     ],
 
     journal_shortcut_js: [
